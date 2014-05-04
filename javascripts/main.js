d3b7eaa7d9562bd7f04f3091acc4d37e8feb8fc1
Ankit Soni <ankitsoni9@gmail.com>
2014-05-04 01:53:20 -0400

    test js canvas code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('This would be the main JS file.');
+
+var canvas = document.getElementById('canvas');
+if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillRect (10, 10, 55, 50);
+
+  ctx.filLStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect = (30,30,55,50);
+}
