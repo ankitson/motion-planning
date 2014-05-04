9b8bde1260f1f611e030b91b0646670e9db68645
Ankit Soni <ankitsoni9@gmail.com>
2014-05-04 17:17:51 -0400

    implemented (untested) most of trapezoidal map algorithm

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,16 @@
-console.log('This would be the main JS file.');
+console.log('yolo');
+/*$(document).ready(function() {
+  console.log('This would be the main JS file.');
+  alert('lol');
 
-var canvas = document.getElementById('canvas');
-if (canvas.getContext) {
-  var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect (10, 10, 55, 50);
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
 
-  ctx.filLStyle = "rgba(0, 0, 200, 0.5)";
-  ctx.fillRect = (30,30,55,50);
-}
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect = (30,30,55,50);
+  }
+});*/
